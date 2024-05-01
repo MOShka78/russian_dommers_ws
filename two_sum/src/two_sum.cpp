9f7e4d70b4e834e8315110c85209696e22555a7b
v.eliseev <Abhaz123>
2024-05-01 12:11:45 +0300

    Добавил реализацию, но теперь надо поменять тесты

--- two_sum/src/two_sum.cpp
+++ two_sum/src/two_sum.cpp
@@ -1,10 +1,19 @@
 #include <two_sum.h>
 std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
-  std::vector<int> out;
-  for (int current = 0; current < nums.size(); current++) {
-    std::find(nums[current], nums.end(), 2);
+  unordered_map<int, int> mp;
+  for (int i = 0; i < nums.size(); i++) {
+    // находим число-дополнение
+    int complement = target - nums[i];
+    // если находим дополнение в unordered_map
+    if (mp.find(complement) != mp.end()) {
+      // возвращаем его индекс и текущий индекс
+      return {mp[complement], i};
+    }
+    // добавляем текущее число и его индекс в unordered_map
+    mp[nums[i]] = i;
   }
-  return out;
+  // если нет решения, выбрасываем исключение
+  throw invalid_argument("No two sum solution");
 }
 
 //[6, 7, 12, 5, 4] target = 11
