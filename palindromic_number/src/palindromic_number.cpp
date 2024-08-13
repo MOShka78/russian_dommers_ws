bf4078241c4877f1c74a64238db1f4133a0a033c
Вадим Елисеев <v.eliseev@rtc.ru>
2024-08-13 14:58:42 +0300

    решение для palindromic_number

--- palindromic_number/src/palindromic_number.cpp
+++ palindromic_number/src/palindromic_number.cpp
@@ -1,6 +1,15 @@
 #include <palindromic_number.hpp>
 
-bool Solution::longestPalindrome(int x) {
+bool Solution::isPalindrome(int x) {
   std::string str_x = std::to_string(x);
-  std::reverse(str_x.begin(), str_x.end());
+  std::string reverse_str_x = str_x;
+
+  std::reverse(reverse_str_x.begin(), reverse_str_x.end());
+
+  for (size_t cur_num = 0; cur_num < str_x.size(); cur_num++) {
+    if (str_x[cur_num] != reverse_str_x[cur_num]) {
+      return false;
+    }
+  }
+  return true;
 }
