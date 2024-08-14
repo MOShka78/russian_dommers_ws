e47ee9edd19e9c3984bbd9e01ccc597fd1d901e4
Вадим Елисеев <v.eliseev@rtc.ru>
2024-08-14 14:00:16 +0300

    исправил нейминг в cmake. добавил clang_format google

--- string_to_integer/src/string_to_integer.cpp
+++ string_to_integer/src/string_to_integer.cpp
@@ -5,16 +5,16 @@ int Solution::myAtoi(std::string s) {
   long long ans = 0;
   int sign = 1;
   int i = 0;
-  while (i < n && s[i] == ' ') {
+  while ((i < n) && (s[i] == ' ')) {
     i++;
   }
 
-  if (i < n && s[i] == '-' || s[i] == '+') {
+  if (((i < n) && (s[i] == '-')) || (s[i] == '+')) {
     sign = (s[i] == '-') ? -1 : 1;
     i++;
   }
 
-  while (i < n && isdigit(s[i])) {
+  while ((i < n) && isdigit(s[i])) {
     ans = ans * 10 + (s[i] - '0');
 
     if (ans > INT32_MAX) {
